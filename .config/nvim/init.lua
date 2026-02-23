686b3d22bec04a5448d5081c11605bae228b1648
Jingchao <alswlx@gmail.com>
2026-02-23 14:47:13 +0800

    feat: markview no sign

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,21 @@
-# legacy
+-- legacy
 local vimrc = vim.fn.stdpath("config") .. "/legacy.vim"
 vim.cmd.source(vimrc)
 
 vim.g.neovide_input_ime = true
+
+require("markview").setup({
+    markdown = {
+        headings = {
+            heading_1 = { sign = "" },
+            heading_2 = { sign = "" },
+            heading_3 = { sign = "" },
+            heading_4 = { sign = "" },
+            heading_5 = { sign = "" },
+            heading_6 = { sign = "" },
+        },
+        code_blocks = {
+            sign = false
+        }
+    },
+})
