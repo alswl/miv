037e3384729ed2eeb84f32204453dd80aa693e84
Jingchao <alswlx@gmail.com>
2026-08-11 15:55:18 +0800

    feat: diffview

--- .config/nvim/lua/config/diffview.lua
+++ .config/nvim/lua/config/diffview.lua
@@ -1,13 +1,30 @@
 local M = {}
 
+local function default_branch()
+    for _, branch in ipairs({ "origin/main", "origin/master" }) do
+        if vim.fn.system({ "git", "rev-parse", "--verify", "--quiet", branch }) ~= "" then
+            return branch
+        end
+    end
+
+    vim.notify("Neither origin/main nor origin/master exists", vim.log.levels.WARN)
+end
+
+local function compare_with_default_branch()
+    local branch = default_branch()
+    if branch then
+        vim.cmd("DiffviewOpen " .. branch .. "...HEAD")
+    end
+end
+
 function M.setup()
     vim.keymap.set("n", "<leader>dd", "<Cmd>DiffviewOpen<CR>", {
         silent = true,
         desc = "Open Git diff",
     })
-    vim.keymap.set("n", "<leader>du", "<Cmd>DiffviewOpen @{upstream}...HEAD<CR>", {
+    vim.keymap.set("n", "<leader>du", compare_with_default_branch, {
         silent = true,
-        desc = "Compare with upstream",
+        desc = "Compare with origin/main or origin/master",
     })
     vim.keymap.set("n", "<leader>dq", "<Cmd>DiffviewClose<CR>", {
         silent = true,
