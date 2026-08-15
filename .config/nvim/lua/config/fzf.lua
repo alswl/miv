281ef4ceb9411acd39f8f67a366813ddd7d5cc3d
Jingchao <alswlx@gmail.com>
2026-08-16 00:17:38 +0800

    feat: add project/buffer-dir find-files keymaps for fzf
    C-p now defaults to the current buffer's directory; leader-ff/leader-fF
    add explicit project-root and buffer-dir file finders.

--- .config/nvim/lua/config/fzf.lua
+++ .config/nvim/lua/config/fzf.lua
@@ -11,9 +11,34 @@ function M.setup()
         global = { pickers = { { "files" }, { "buffers", prefix = "$" }, { "oldfiles", prefix = "#" } } },
     })
 
-    vim.keymap.set("n", "<C-p>", fzf.global, {
+    local function project_root()
+        return vim.fs.root(0, ".git") or vim.fn.getcwd()
+    end
+
+    local function buffer_dir()
+        local bufname = vim.api.nvim_buf_get_name(0)
+        return bufname ~= "" and vim.fn.fnamemodify(bufname, ":p:h") or vim.fn.getcwd()
+    end
+
+    vim.keymap.set("n", "<C-p>", function()
+        fzf.global({ cwd = buffer_dir() })
+    end, {
         silent = true,
         desc = "Find files / mru",
+    })
+
+    vim.keymap.set("n", "<leader>ff", function()
+        fzf.files({ cwd = project_root() })
+    end, {
+        silent = true,
+        desc = "Find files (project)",
+    })
+
+    vim.keymap.set("n", "<leader>fF", function()
+        fzf.files({ cwd = buffer_dir() })
+    end, {
+        silent = true,
+        desc = "Find files (current buffer dir)",
     })
 end
 
